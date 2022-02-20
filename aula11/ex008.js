9764b96ab2958babf22d518f638aeb05fe191a9f
Gaziero <gazierolucas@gmail.com>
2022-02-20 11:41:05 -0300

    New exercises

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`Your travelling speed is is ${vel}Km/h`)
+
+if (vel > 50) {
+    console.log("You've got a fine")
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var country = "Brasil"
+console.log(`Living in ${country}`)
+if (country == "Brasil") {
+    console.log("You are brazilian")
+}
+else {
+    console.log("You are a gringo")
+}
