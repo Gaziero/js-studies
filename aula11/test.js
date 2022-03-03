3a3b59a562134444716c50c7e87dbe39dcc681af
Gaziero <gazierolucas@gmail.com>
2022-03-03 09:21:31 -0300

    Started RocketSeat's project

--- aula11/test.js
+++ aula11/test.js
@@ -1,20 +1,11 @@
-document.getElementById("holder", holder);
-document.getComputedStyle(holder, holderStyle);
-document.getElementById("square", square);
-document.getComputedStyle(square, squareStyle);
-
-window.document.getElementById("square").addEventListener("mouseover", mouseOver);
-window.document.getElementById("square").addEventListener("mouseout", mouseOut);
-
 var rowPos = holderStyle.getPropertyValue("align-items");
 var colPos = holderStyle.getPropertyValue("justify-content");
 
-function clicked() {
-    holder.style.justifyContent = "end"
-    square.style.width = "100%"
-    square.style.backgroundColor = "red"
+function moveSquare() {
+    holder.style.justifyContent = "end";
+    square.style.width = "100%";
+    square.style.backgroundColor = "red";
 }
 
-function mouseOver() {
-    document.getElementById("square").style.backgroundColor = "green"
-}
+const square = document.getElementById("square");
+square.addEventListener("click", moveSquare)
